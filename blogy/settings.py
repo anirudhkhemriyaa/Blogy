f39d0003c155998f2f83e4806f6cd342b565a7f5
anirudhkhemriyaa <anirudhakhemriya06@gmail.com>
2025-12-06 10:31:27 +0530

    models added

--- blogy/settings.py
+++ blogy/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"] 
 
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
