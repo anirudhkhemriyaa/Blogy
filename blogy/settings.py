97902f6e2a11e8a40d7010fd8ac09b9a782e8f52
anirudhkhemriyaa <anirudhakhemriya06@gmail.com>
2025-12-07 21:03:33 +0530

    Update setting

--- blogy/settings.py
+++ blogy/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_n#hsp+l(hf5@y-$daqg#=43c7w$84z#5zrr@eqfp_8birtla0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['anirudhahoonna.pythonanywhere.com']
 
 
 # Application definition
@@ -120,8 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / "static" 
-STATICFILES_DIRS = [BASE_DIR / "static"] 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 
 
 MEDIA_URL = '/media/'
